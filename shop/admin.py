d7858d732e3e9c73f46ad18d36a0754862d5db50
artem <zhiruyev_a@bankrbk.kz>
2023-12-24 20:50:23 +0600

    feat(shop): new models and migrations added

--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,32 @@
 from django.contrib import admin
 
-from shop.models import Item
+from shop.models import (
+    Item,
+    Order,
+    Discount,
+    Tax,
+)
 
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'price_for_view',)
+    search_fields = ('name',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id',)
+    search_fields = ('id',)
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Tax)
+class TaxAdmin(admin.ModelAdmin):
+    list_display = ('name',)
     search_fields = ('name',)
--- shop/models.py
+++ shop/models.py
@@ -14,3 +14,40 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Discount(models.Model):
+    name = models.CharField(max_length=100)
+    stripe_id = models.CharField()
+
+    class Meta:
+        verbose_name = _('Скидка')
+        verbose_name_plural = _('Скидки')
+
+    def __str__(self):
+        return self.name
+
+
+class Tax(models.Model):
+    name = models.CharField(max_length=100)
+    stripe_id = models.CharField()
+
+    class Meta:
+        verbose_name = _('Налог')
+        verbose_name_plural = _('Налоги')
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    items = models.ManyToManyField(Item)
+    discount = models.ForeignKey(Discount, on_delete=models.SET_NULL, null=True, blank=True)
+    tax = models.ForeignKey(Tax, on_delete=models.SET_NULL, null=True, blank=True)
+
+    class Meta:
+        verbose_name = _('Заказ')
+        verbose_name_plural = _('Заказы')
+
+    def __str__(self):
+        return self.id
